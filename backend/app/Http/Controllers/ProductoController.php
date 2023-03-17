9ace5e0f252268fc04e16ad308986744140a7d61
Zami_Tulande <hulmartulande@gmail.com>
2023-03-17 17:04:16 -0500

    libreria useSWR

--- backend/app/Http/Controllers/ProductoController.php
+++ backend/app/Http/Controllers/ProductoController.php
@@ -14,7 +14,8 @@ class ProductoController extends Controller
     public function index()
     {
         //
-        return new ProductoCollection(Producto::where('disponible',1)->orderBy('id', 'DESC')->paginate(10));
+        //return new ProductoCollection(Producto::where('disponible',1)->orderBy('id', 'DESC')->paginate(10));
+        return new ProductoCollection(Producto::where('disponible', 1)->orderBy('id', 'DESC')->get());
     }
 
     /**
--- frontend/src/views/Inicio.jsx
+++ frontend/src/views/Inicio.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import useSWR from 'swr'
 import Productos from '../components/Productos'
+import clienteAxios from "../config/axios"
 import {productos as data} from '../data/productos'
 import useCafe from '../hooks/useCafe'
 
 const Inicio = () => {
 
   const {categoriaActual}= useCafe()
-  const productos = data.filter(producto=> producto.categoria_id === categoriaActual.id)
+
+  //consulta SWR
+  const fetcher =()=>clienteAxios('/api/productos').then(data =>data.data)
+  const {data, error, isLoading} = useSWR('/api/productos', fetcher,{
+    refreshInterval:1000
+  })
+  if(isLoading) return 'Cargando...'
+  const productos = data.data.filter(producto=> producto.categoria_id === categoriaActual.id)
   return (
     <>
       <h1 className='text-4xl font-black'>{categoriaActual.nombre}</h1>
