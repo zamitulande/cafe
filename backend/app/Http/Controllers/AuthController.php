f275ccab334de6f78410f566338550c2f697dc3a
Zami_Tulande <hulmartulande@gmail.com>
2023-03-18 17:05:05 -0500

    agregando usuario a base de datos y obteniendo token en navegador-network-preview

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
    public function register(RegistroRequest $request){
-        $data = $request->validate();
+        $data = $request->validated();
+
+        //crear usario
+        $user = User::create([
+         'name' => $data['name'],
+         'email' => $data['email'],
+         'password' => bcrypt($data['password'])
+        ]);
+        //retornar una respuesta
+        return [
+          'token' => $user->createToken('token')->plainTextToken,
+          'user' => $user
+        ];
    }
    public function login(Request $request){
 
