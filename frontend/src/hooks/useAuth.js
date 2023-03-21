6d262459bd659474fc43085874529b2b5f6e6575
Zami_Tulande <hulmartulande@gmail.com>
2023-03-21 14:31:18 -0500

    redireccionando una vez la autenticacion se valide

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,12 +1,32 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useSWR from 'swr'
 import clienteAxios from "../config/axios"
 
 export const useAuth = ({middleware, url}) => {
+
+    const token = localStorage.getItem('AUTH_TOKEN')
+
+    const navigate = useNavigate()
+
+    const {data: user, error, mutate} = useSWR('/api/user', ()=>
+        clienteAxios('/api/user',{
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.data)
+        .catch(error=>{
+            throw Error(error?.response?.data?.errors)
+        })
+    )
     
     const login = async (datos, setErrores)=>{
         try {
             const {data}= await clienteAxios.post('/api/login', datos)
-            localStorage.setItem('AUTH TOKEN', data.token)
+            localStorage.setItem('AUTH_TOKEN', data.token)
             setErrores([])
+            await mutate()
         } catch (error) {
             console.log(error)
             setErrores(Object.values(error.response.data.errors))
@@ -20,6 +40,14 @@ export const useAuth = ({middleware, url}) => {
     const logout = ()=>{
         
     }
+    console.log(user)
+    console.log(error)
+
+    useEffect(()=>{
+        if(middleware === 'guest' && url && user){
+            navigate(url)
+        }
+    }, [user, error])
 
     return{
         login,
